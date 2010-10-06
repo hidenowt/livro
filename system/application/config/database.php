d1fb6086c22faf46a0aecaeceeeee5c9592ff9f3
Herminio Torres <herminiocesar@gmail.com>
2010-10-06 00:21:49 -0300

    5.2 habilitando o codeigniter para trabalhar com o banco de dados, pag: 58-59

--- system/application/config/database.php
+++ system/application/config/database.php
@@ -38,9 +38,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
-$db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['username'] = "codeigniter";
+$db['default']['password'] = "12345";
+$db['default']['database'] = "livro";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
